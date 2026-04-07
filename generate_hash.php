ad2ed36c0be54492370f1e75c2644190679d95a7
2011200620112006 <raneriya06@gmail.com>
2026-04-07 20:35:20 +0530

    Delete generate_hash.php

--- generate_hash.php
+++
@@ -1,6 +0,0 @@
-<?php
-$password = "admin123";  // Change this to your desired password
-$hashed_password = password_hash($password, PASSWORD_BCRYPT);
-echo "Your hashed password is: <br><br>";
-echo "<strong>" . $hashed_password . "</strong>";
-?>
